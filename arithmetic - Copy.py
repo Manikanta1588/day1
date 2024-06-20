344b7d271eb97125b853cef159d95e36c4b54117
Manikanta1588 <suryadevaramanikanta017@gmail.com>
2024-06-20 10:16:35 +0530

    Add files via upload

---
+++ arithmetic - Copy.py
@@ -0,0 +1,7 @@
+a=int(input("enter the number:"))
+b=int(input("enter the number :"))
+
+print("Adding two numbers which is:",a+b)
+print("subtraction two numbers which is:",a-b)
+print("multiplying two numbers which is:",a%b)
+
---
+++ if-else.py
@@ -0,0 +1,7 @@
+
+a=int(input("enter your age for voting:"))
+if a>18:
+   print("your eligible to vote")
+
+else:
+     print("your not allowed to vote") 
